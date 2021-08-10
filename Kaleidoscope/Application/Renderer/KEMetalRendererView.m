5ae9fc8d897d5b7193bb8dbbcb4294f55af9110c
CrowFea <wendaoliu40@gmail.com>
2021-08-10 16:49:57 +0800

    feat: init pipeline

--- Kaleidoscope/Application/Renderer/KEMetalRendererView.m
+++ Kaleidoscope/Application/Renderer/KEMetalRendererView.m
@@ -9,8 +9,9 @@
 
 @interface KEMetalRendererView()
 
-
-
+@property (nonatomic, strong) id<MTLFunction> vertexFunc;
+@property (nonatomic, strong) id<MTLFunction> fragmentFunc;
+@property (nonatomic, strong) id<MTLRenderPipelineState> pipelineState;
 @end
 
 @implementation KEMetalRendererView
@@ -25,6 +26,23 @@
 }
 
 
+- (void)loadShadersWithVertexName: (NSString*)vertex FragmentName: (NSString*)fragment {
+    NSBundle* bundle = [NSBundle mainBundle];
+    id<MTLLibrary> defalutLib = [self.device newDefaultLibraryWithBundle:bundle error:nil];
+    self.vertexFunc = [defalutLib newFunctionWithName:vertex];
+    self.fragmentFunc = [defalutLib newFunctionWithName:fragment];
+    return;
+}
+
+- (void)setupPipeline {
+    MTLRenderPipelineDescriptor* pipeLineDes = [[MTLRenderPipelineDescriptor alloc] init];
+    pipeLineDes.vertexFunction = self.vertexFunc;
+    pipeLineDes.fragmentFunction =  self.fragmentFunc;
+    pipeLineDes.colorAttachments[0].pixelFormat = self.colorPixelFormat;
+    self.pipelineState = [self.device newRenderPipelineStateWithDescriptor:pipeLineDes error:nil];
+    
+    return;
+}
 
 
 #pragma mark - MTKViewDelegate
